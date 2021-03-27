7b2604f370c6076ebf9e263810d3f61d89a9f4ec
Piotr <s21911@pjwstk.edu.pl>
2021-03-27 21:53:54 +0100

    one change

--- Laboratorium 1/zad 5.php
+++ Laboratorium 1/zad 5.php
@@ -1,10 +1,10 @@
 <?php
 if(isset($_POST["show"])){
     $double=$_POST["double"];
-    $double = "napis1 napis2";
-    $napis = explode(" ",$double,2);
+    $napis = explode(",",$double);
+    
     echo "<br>";
-    echo "$napis[1],&nbsp,$napis[0]";
+    echo "%$napis[1],&nbsp,$napis[0]%\$#";
     
     
     
